49cd8268b6a81d1e28c2f014e3977e19205f6e98
Fumiya-Kito <cfbc.pearl@icloud.com>
2025-01-26 15:57:25 +0900

    feat: add jockey, trainer, owner column to df

--- src/create_rawdf.py
+++ src/create_rawdf.py
@@ -19,12 +19,24 @@ def create_results(html_path_list: list[Path]) -> pd.DataFrame:
       race_id = html_path.stem
       html = f.read()
       df = pd.read_html(html)[0]
-      # horse_id列を追加
-      horse_table_soup = BeautifulSoup(html, "lxml").find(
+      soup = BeautifulSoup(html, "lxml").find(
         "table", class_="race_table_01 nk_tb_common"
       )
-      horse_id_list = get_ids_in_table(soup_table=horse_table_soup, target="horse")
+      # horse_id列を追加
+      horse_id_list = get_ids_in_table(soup_table=soup, target="horse", id_length=10)
       df["horse_id"] = horse_id_list  
+      
+      # ジョッキーIDを取得 
+      jockey_id_list = get_ids_in_table(soup_table=soup, target="jockey", id_length=5)
+      df["jockey_id"] = jockey_id_list
+      
+      # 調教師IDを取得 
+      trainer_id_list = get_ids_in_table(soup_table=soup, target="trainer", id_length=5)
+      df["trainer_id"] = trainer_id_list
+      
+      # オーナーIDを取得 
+      owner_id_list = get_ids_in_table(soup_table=soup, target="owner", id_length=6)
+      df["owner_id"] = owner_id_list
       
       # index列追加
       df.index = [race_id] * len(df) # race_idをindexに(list * int = 繰り返しになる)
@@ -37,10 +49,10 @@ def create_results(html_path_list: list[Path]) -> pd.DataFrame:
 
 
 
-def get_ids_in_table(soup_table: BeautifulSoup, target: str) -> list[str]:
+def get_ids_in_table(soup_table: BeautifulSoup, target: str, id_length: int) -> list[str]:
   target_a_list = soup_table.find_all("a", href=re.compile(rf"^/{target}/"))
   target_id_list = []
   for a in target_a_list:
-    target_id = re.findall(r"\d{10}", a["href"])[0]
+    target_id = re.findall(rf"\d{{{id_length}}}", a["href"])[0]
     target_id_list.append(target_id)
   return target_id_list
